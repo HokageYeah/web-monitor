b6343c72d8ad4c7eeb1e154a4f61c742eeb87a78
yuye <18838280615@163.com>
2023-08-04 08:37:53 +0800

    代码提交

---
+++ packages/code/src/types/options.ts
@@ -0,0 +1,16 @@
+interface Pv {
+  core?: boolean; // 是否发送页面跳转相关数据
+}
+// sdk 配置
+export interface InitOptions {
+  dsn: string; // 上报地址（上传地址）
+  appName: string; // 应用名称
+  appCode?: string; // 应用code
+  appVersion?: string; // 应用版本
+  userUuid?: string; // 用户id(外部填充进来的id)
+}
+
+export interface RecordEventScope {
+  scope: string;
+  eventList: any[];
+}
